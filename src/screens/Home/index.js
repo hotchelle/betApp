2eb943e9072a6beb0647a5c95bc5a662bcf29b02
Abdulla Sakallah <iaboodws11@gmail.com>
2022-04-23 19:13:29 -0500

    update3

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,38 @@
-import { View, Text,ScrollView,TextInput,TouchableOpacity } from 'react-native'
-import React from 'react'
-import styles from './styles';
+import { View, Text, StyleSheet, ImageBackground, ScrollView, TextInput, TouchableOpacity } from 'react-native'
+import * as React from 'react';
+import styles from './styles'
 import { COLORFONTS } from '../../../Constants/theme'
 
 
-const HomeScreen = ( {navigation} ) => {
-    return (
-      <View>
-  
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
 
-      </View>
-    )
+        <ScrollView>
+          <View style={styles.topContainer}>
+            <Text style={styles.title}>*My Ace Bet*</Text>
+          </View>
+          
+          <View style={styles.anyContainer}>
+            <Text style={styles.title}>Enter  Pin</Text>
+          </View>
+
+          <View style={styles.dataContainer}>
+            <TextInput style={styles.textinput} placeholderTextColor={COLORFONTS.white} />
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={() => navigation.navigate('Bid')}>
+              <View style={styles.firstButton}>
+                <Text style={styles.buttonText}>Ace</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+        </ScrollView>
+    </View>
+  )
 }
+
+
 
 export default HomeScreen;
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,17 +1,85 @@
 import { StyleSheet } from "react-native";
-
+import { COLORFONTS, SIZEFONTS } from "../../../Constants/theme";
 
 const styles = StyleSheet.create(
     {
         container:
         {
             flex: 1,
+            backgroundColor: '#001F3F',
+        },
+        topContainer:
+        {
+            marginTop: 100,
+            alignItems: 'center'
+
+        },
+        anyContainer:
+        {
+            flex: 2,
+            alignItems: 'center',
+            marginTop: 150,
+            
+
+        },
+        sepContainer:
+        {
+            flex: 2,
+            alignItems: 'center'
+
+        },
+        title:
+        {
+            color: COLORFONTS.white,
+            fontSize: SIZEFONTS.h1 *1.5,
+            fontWeight: 'bold'
+        },
+        subTitle:
+        {
+            color: COLORFONTS.white,
+            fontSize: SIZEFONTS.h4,
+            paddingTop: 6
+        },
+        dataContainer:
+        {
+            marginTop: 80,
+        },
+        textinput:
+        {
+            flex: 2,
+            color: COLORFONTS.white,
+            fontSize: SIZEFONTS.h3,
+            borderBottomColor: COLORFONTS.lightgrey,
+            borderBottomWidth: 1,
+            paddingVertical: 1,
+            marginHorizontal: 100,
+            marginVertical: 2,
+
+        },
+        buttonContainer:
+        {
+            marginTop: 50
+            
+        },
+        firstButton:
+        {
+            backgroundColor: '#2E8B57',
+            padding: 20,
+            marginHorizontal: 70,
+            borderRadius: 30,
             justifyContent: 'center',
             alignItems: 'center',
+            marginVertical: 1,
         },
-
+        buttonText:
+        {
+            marginTop: 10,
+            flex: 2,
+            color: COLORFONTS.white,
+            fontWeight: 'bold',
+            fontSize: SIZEFONTS.h4,
+        },
     }
-)
-
+);
 
 export default styles;
